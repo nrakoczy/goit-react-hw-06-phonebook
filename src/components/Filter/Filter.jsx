9f498e1ff4a1ad5b4d95d0ea16355f226eeb0773
NataliaRakoczy <natalia.rakoczy54@gmail.com>
2023-08-02 08:08:25 +0200

    added code

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,20 @@
 import { Input, Label } from './Filter.styled';
-import { setFilter } from 'redux/filterSlice'; 
-import { useDispatch, useSelector } from 'react-redux'; 
-import { getFilter } from 'redux/selectors'; 
+import { setFilter } from 'redux/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFilter } from 'redux/selectors';
 
 export const Filter = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(getFilter); 
+  const currentFilter = useSelector(getFilter);
+
+  const handleFilterChange = event => {
+    dispatch(setFilter(event.target.value.trim()));
+  };
 
   return (
     <Label>
       Find contacts by name
-      <Input
-        type="text"
-        value={filter}
-        onChange={event => dispatch(setFilter(event.target.value.trim()))} 
-      />
+      <Input type="text" value={currentFilter} onChange={handleFilterChange} />
     </Label>
   );
 };
-
-
